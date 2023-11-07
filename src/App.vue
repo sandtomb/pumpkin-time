4023458bc8537d4b48946c009771db5dabfc2543
sandtomb <minusben@gmail.com>
2023-11-06 23:59:16 -0500

    More pumpkin updates :)

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,29 @@ Run Carve Pumpkin
 <script lang="ts">
 import { ref, type Ref } from 'vue'
 
+const POSSIBLE_PUMPKIN_DESIGNS = [
+  'Ghost', 'Elf', 'Wizard', 'Archer', 'Knight', 'Pikachu', 'Eevee', 'Charmander', 'Scary Face', 'Happy Face', 'Sad Face'
+]
+
+class Pumpkin {
+  id: string
+  design: string
+
+  constructor() {
+    this.id = crypto.randomUUID()
+    this.design = POSSIBLE_PUMPKIN_DESIGNS[(Math.floor(Math.random() * POSSIBLE_PUMPKIN_DESIGNS.length))]
+  }
+
+  toString(): string {
+    return 'Pumpkin ID: ' + this.id + ' : with design: ' + this.design 
+  }
+}
+
 interface State {
   pickPumpkin(): void,
   putPumpkinBack(): void,
   carvePumpkin(): void,
+  submitPumpkin(): void,
 }
 
 class NoPumpkinState implements State {
@@ -45,6 +64,9 @@ class NoPumpkinState implements State {
   }
   carvePumpkin = (): void => {
     console.log('You do not have a pumpkin to carve!')
+  }
+  submitPumpkin(): void {
+    console.log('You do not have have a pumpkin to submit!')
   }
 }
 
@@ -59,30 +81,63 @@ class HasPumpkinState implements State {
   carvePumpkin = (): void => {
     console.log('You carve your pumpkin!')
   }
+  submitPumpkin(): void {
+    console.log('You must have a carved pumpkin for eligible submission!')
+  }
 }
 
-type PumpkinContestState = (NoPumpkinState | HasPumpkinState)
+class CarvedPumpkinState implements State {
+  constructor() { }
+  pickPumpkin = (): void => {
+    console.log('You already have a pumpkin')
+  }
+  putPumpkinBack = (): void => {
+    console.log('You put your pumpkin back into the patch')
+  }
+  carvePumpkin = (): void => {
+    console.log('You carve your pumpkin!')
+  }
+  submitPumpkin(): void {
+    console.log('You successfullly submit your pumpkin')
+  }
+}
+
+type PumpkinContestState = (NoPumpkinState | HasPumpkinState | CarvedPumpkinState)
 
 class PumpkinContest {
+  MAX_PUMPKINS: number
+
   noPumpkinState: NoPumpkinState
   hasPumpkinState: HasPumpkinState
+  carvedPumpkinState: CarvedPumpkinState
 
-  pumpkinCount: number
+  pumpkins: Array<Pumpkin>
   state: PumpkinContestState
 
-  constructor(count: number) {
+  constructor() {
+    this.MAX_PUMPKINS = 5
     this.noPumpkinState = new NoPumpkinState()
     this.hasPumpkinState = new HasPumpkinState()
+    this.carvedPumpkinState = new CarvedPumpkinState()
 
-    this.pumpkinCount = count
+    this.pumpkins = []
     this.state = new NoPumpkinState()
+  }
+
+  pickWinner() : void {
+    if (this.pumpkins.length >= this.MAX_PUMPKINS) {
+      const winner: Pumpkin = this.pumpkins[(Math.floor(Math.random() * this.pumpkins.length))]
+      this.pumpkins = []
+      alert('Winner is: ' + winner.toString())
+    } else {
+      alert('Not enough pumpkins have been submitted!')
+    }
   }
 }
 
 export default {
   setup() {
-    const MAX_PUMPKINS: number = 5
-    const pumpkinContest:Ref<PumpkinContest> = ref(new PumpkinContest(0))
+    const pumpkinContest:Ref<PumpkinContest> = ref(new PumpkinContest())
 
     const pickPumpkin = (): void => {
 
@@ -98,11 +153,10 @@ export default {
     }
 
     const pickWinner = (): void => {
-
+      return pumpkinContest.value.pickWinner()
     }
 
     return {
-      MAX_PUMPKINS,
       pickPumpkin,
       putPumpkinBack,
       pumpkinContest,
