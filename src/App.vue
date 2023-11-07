8f626c22c6128da99ccc9406493b5de8c5ee65d8
sandtomb <minusben@gmail.com>
2023-11-06 22:58:12 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+
+<button
+  @click="carvePumpkin"
+>
+  Pick Pumpkin
+</button>
+
+<button
+ @click="carvePumpkin"
+>
+Put Pumpkin Back
+</button>
+
+<button
+ @click="carvePumpkin"
+>
+Run Carve Pumpkin
+</button>
+
+<button
+ @click="carvePumpkin"
+>
+  Pick Winner
+</button>
+
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const MAX_PUMPKINS: number = 5
+
+    const pumpkinContest = ref({})
+
+    const pickPumpkin = (): void => {
+
+    }
+
+    const putPumpkinBack = (): void => {
+
+    }
+
+    const carvePumpkin = (): void => {
+      // alert(pumpkinContest.state.carvePumpkin())
+      alert('hi')
+    }
+
+    const pickWinner = (): void => {
+
+    }
+
+    return {
+      MAX_PUMPKINS,
+      pickPumpkin,
+      putPumpkinBack,
+      pumpkinContest,
+      carvePumpkin,
+      pickWinner,
+    }
+  }
+}
+  
+</script>
+
+<style scoped>
+
+</style>
