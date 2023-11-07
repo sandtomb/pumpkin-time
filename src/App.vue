ee78329d8000678089aa1ceec2af9826e3a0f26a
sandtomb <minusben@gmail.com>
2023-11-06 23:32:11 -0500

    Updates for TypeScript

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,62 @@ Run Carve Pumpkin
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
+
+interface State {
+  pickPumpkin(): void,
+  putPumpkinBack(): void,
+  carvePumpkin(): void,
+}
+
+class NoPumpkinState implements State {
+  constructor() { }
+  pickPumpkin = (): void => {
+    console.log('You have picked a pumpkin')
+  }
+  putPumpkinBack = (): void => {
+    console.log('You do not have a pumpkin to put back!')
+  }
+  carvePumpkin = (): void => {
+    console.log('You do not have a pumpkin to carve!')
+  }
+}
+
+class HasPumpkinState implements State {
+  constructor() { }
+  pickPumpkin = (): void => {
+    console.log('You already have a pumpkin')
+  }
+  putPumpkinBack = (): void => {
+    console.log('You put your pumpkin back into the patch')
+  }
+  carvePumpkin = (): void => {
+    console.log('You carve your pumpkin!')
+  }
+}
+
+type PumpkinContestState = (NoPumpkinState | HasPumpkinState)
+
+class PumpkinContest {
+  noPumpkinState: NoPumpkinState
+  hasPumpkinState: HasPumpkinState
+
+  pumpkinCount: number
+  state: PumpkinContestState
+
+  constructor(count: number) {
+    this.noPumpkinState = new NoPumpkinState()
+    this.hasPumpkinState = new HasPumpkinState()
+
+    this.pumpkinCount = count
+    this.state = new NoPumpkinState()
+  }
+}
 
 export default {
   setup() {
     const MAX_PUMPKINS: number = 5
-
-    const pumpkinContest = ref({})
+    const pumpkinContest:Ref<PumpkinContest> = ref(new PumpkinContest(0))
 
     const pickPumpkin = (): void => {
 
